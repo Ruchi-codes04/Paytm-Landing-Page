c5cf61634874d2bc94059097d79d2f4e1537806e
Ruchi-codes04 <p.ruchika142003@gmail.com>
2024-09-21 13:49:34 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const headerBtnImg = document.getElementById("header-btn-img");
+
+const onHover = (e) => (headerBtnImg.src = "./Images/user.svg");
+
+const onLeave = (e) => (headerBtnImg.src = "./Images/user-default.svg");
